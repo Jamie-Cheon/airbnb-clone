74ef0264d45cd68977f6d9e2c3ad3cb5c0ad6ced
Jamie-Cheon <jamiecheon55@gmail.com>
2020-12-16 02:20:36 -0800

    finishing HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Room
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = ("created",)
+    template_name = "rooms/room_list.html"
+    context_object_name = "rooms"
+
+
